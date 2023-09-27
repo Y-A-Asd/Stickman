da8ac3c3124879e8b55bf8c4fb5914b5e9de5b54
flatl <yosofasady2@gmail.com>
2023-09-27 11:30:09 +0330

    DONE

--- command/Command_Manager.py
+++ command/Command_Manager.py
@@ -2,67 +2,55 @@ from core.Actions import Damage, Money_Status, Army_Status, Add_Troops, Enemy_St
 from abc import ABC,abstractmethod
 
 
-class StateManager(ABC):
+class Command(ABC):
     @abstractmethod
-    def execute(self, manager, args):
+    def execute(self, *args):
         pass
 
 
-class AddTroopsState(StateManager):
-    def execute(self, manager, args):
-        troops_type, timestamps = args
-        result = Add_Troops.add_troop(troops_type, timestamps)
-        print(result)
+class AddTroopsCommand(Command):
+    def execute(self, troops_type, timestamps):
+        return Add_Troops.add_troop(troops_type, timestamps)
 
 
-class DamageState(StateManager):
-    def execute(self, manager, args):
-        idx, d, timestamps = args
-        result = Damage.damage(idx, d, timestamps)
-        print(result)
+class DamageCommand(Command):
+    def execute(self, idx, d, timestamps):
+        return Damage.damage(idx, d, timestamps)
 
 
-class EnemyStatusState(StateManager):
-    def execute(self, manager, args):
-        timestamps = args[0]
-        result = Enemy_Status.enemy_status(timestamps)
-        print(result)
+class EnemyStatusCommand(Command):
+    def execute(self, timestamps):
+        return Enemy_Status.enemy_status(timestamps)
 
 
-class ArmyStatusState(StateManager):
-    def execute(self, manager, args):
-        timestamps = args[0]
-        result = Army_Status.army_status(timestamps)
-        print(result)
+class ArmyStatusCommand(Command):
+    def execute(self, timestamps):
+        return Army_Status.army_status(timestamps)
 
 
-class MoneyStatusState(StateManager):
-    def execute(self, manager, args):
-        timestamps = args[0]
-        result = Money_Status.money_status(timestamps)
-        print(result)
+class MoneyStatusCommand(Command):
+    def execute(self, timestamps):
+        return Money_Status.money_status(timestamps)
 
 
 class CommandManager:
     def __init__(self):
-        self.states = {
-            "add": AddTroopsState(),
-            "damage": DamageState(),
-            "enemy-status": EnemyStatusState(),
-            "army-status": ArmyStatusState(),
-            "money-status": MoneyStatusState()
+        self.commands = {
+            "add": AddTroopsCommand(),
+            "damage": DamageCommand(),
+            "enemy-status": EnemyStatusCommand(),
+            "army-status": ArmyStatusCommand(),
+            "money-status": MoneyStatusCommand()
         }
 
     def command_manager(self, req_list):
         for request in req_list:
-            command = request[0]
+            command_name = request[0]
             args = request[1:]
 
-            if command in self.states:
-                state = self.states[command]
-                state.execute(self, args)
+            if command_name in self.commands:
+                command = self.commands[command_name]
+                result = command.execute(*args)
+                print(result)
             else:
-                print(f"Unknown command: {command}")
-
-
-
+                print(f"Unknown command: {command_name}")
